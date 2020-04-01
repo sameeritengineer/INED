cc8da6d4fec12e1ce1635674e7f7a9b5b81a82bd
sameer <sameer.smartitventures@gmail.com>
2020-04-01 12:13:30 +0530

    some new changes

--- resources/views/web/layouts/sidebar.blade.php
+++ resources/views/web/layouts/sidebar.blade.php
@@ -1,9 +1,8 @@
 <div class="col-md-3 col-sm-3 col-xs-12 left_sidebar">
 	<div class="about_content">
 	<h3 class="title-color margin_top">About</h3>
-	<p>Dr.Sanjaya Satapathy graduated from Veer Surendra Sai Medical College of Sambalpur University,
-India. He completed his Residency in Internal Medicine training at the same college, and then 2 year
-of Hepatology training at the Post Graduate Institute of Medical Education and Research at Chandigarh, India.</p>
+	<p> We strive to bring together the best medical content from major publishers and medical leaders in their specialty, faculty from top training programs to create a large up-to-date training library for healthcare practitioners using fast, easy, effective online learning tools.
+			<a class="read_more_text padding-top-10" href="{{route('web.about-us')}}">Read More</a></p>
 	</div>
 	<div class="sidebar_search">
 		<form class="search_form">
