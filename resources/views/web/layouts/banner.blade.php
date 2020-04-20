1296a664610f2a00a71baa0cb1b3e1bca44e96b6
sameer <sameer.smartitventures@gmail.com>
2020-04-20 12:44:19 +0530

    worked on css for subtitle

--- resources/views/web/layouts/banner.blade.php
+++ resources/views/web/layouts/banner.blade.php
@@ -1,4 +1,4 @@
 <div class="col-md-12 col-sm-12 col-xs-12 inner-banner" style="background-image: url({{asset('web/images/all-inner-pages-headrer4.png')}});">
    <h3 class="inner-page-title"><strong>@yield('bannertitle')</strong></h3>
-   <h3 class="inner-page-title"><strong>@yield('innerbannertitle')</strong></h3>
+   <h3 class="inner-page-sub-title"><strong>@yield('innerbannertitle')</strong></h3>
 </div>
